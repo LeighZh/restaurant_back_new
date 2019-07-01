5de3be99ab9b12187a455e7c4e25a03488053b94
zzRITA <857898559@QQ.com>
2019-07-01 15:07:36 +0800

    meal

--- src/com/znsd/restaurant/servlet/meal/MenuServlet.java
+++ src/com/znsd/restaurant/servlet/meal/MenuServlet.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class MenuServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
@@ -30,27 +30,31 @@ public class MenuServlet extends HttpServlet {
 		MenuServers menu = new MenuServersImpl();
 		String judge = request.getParameter("judge");
 		System.out.println(judge+"66666666666");
-		if(judge==null || judge.equals("")){
-			List<MenuBean> list=menu.query(1,menu.getCount());
-			String josn= JSON.toJSONString(list);
-			response.getWriter().println(josn);
+		if(judge.equals("query")){
+			MenuBean m;
+			menu.query(m);
 		}
-		else if(judge.equals("afterQuery")){
-			this.menuQuery(request, response, menu);
-		}else if(judge.equals("add")){
-			System.out.println("90909090909090909");
-			this.menuAdd(request, response, menu);
-		}else if(judge.equals("delete")){
-			String name=request.getParameter("name");
-			menu.delete(name);
-		}else if(judge.equals("mod")){
-			String name=request.getParameter("usName");
-			String id=request.getParameter("id");
-			System.out.println(name);
-			System.out.println(id);
-			menu.mod(name,Integer.parseInt(id));
-		}
-		
+//		if(judge==null || judge.equals("")){
+//			List<MenuBean> list=menu.query(1,menu.getCount());
+//			String josn=JSON.toJSONString(list);
+//			response.getWriter().println(josn);
+//		}
+//		else if(judge.equals("afterQuery")){
+//			this.menuQuery(request, response, menu);
+//		}else if(judge.equals("add")){
+//			System.out.println("90909090909090909");
+//			this.menuAdd(request, response, menu);
+//		}else if(judge.equals("delete")){
+//			String name=request.getParameter("name");
+//			menu.delete(name);
+//		}else if(judge.equals("mod")){
+//			String name=request.getParameter("usName");
+//			String id=request.getParameter("id");
+//			System.out.println(name);
+//			System.out.println(id);
+//			menu.mod(name,Integer.parseInt(id));
+//		}
+
 	}
 	public void menuAdd(HttpServletRequest request, HttpServletResponse response,MenuServers menu) throws ServletException, IOException{
 		String menuname = request.getParameter("menuname");
@@ -58,23 +62,50 @@ public class MenuServlet extends HttpServlet {
 		String cook = request.getParameter("cook");
 		String menutype = request.getParameter("menutype");
 		String describe = request.getParameter("describe");
-//		String name=menu.add(new MenuBean(menuname,Double.parseDouble(price),cook,menutype,describe));
+		//String name=menu.add(new MenuBean(menuname,Double.parseDouble(price),cook,menutype,describe));
 //		if(name==null||name.equals("")){
 //			System.out.println(name+"***");
 //			request.getRequestDispatcher("afterManage/menu.jsp").forward(request,response);
 //		}
+//	}
+//	public void menuQuery(HttpServletRequest request, HttpServletResponse response,MenuServers menu){
+//		String parameter = request.getParameter("page");
+//		String rows = request.getParameter("rows");
+//		int start = (Integer.parseInt(parameter)-1)*Integer.parseInt(rows);
+//		List<MenuBean> query = menu.query();
+//		try {
+//			int count = menu.getCount();
+//			response.getWriter().print("{\"total\":"+count+",\"rows\":"+JSON.toJSONString(query)+"}");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
-	public void menuQuery(HttpServletRequest request, HttpServletResponse response,MenuServers menu){
-		String parameter = request.getParameter("page");
-		String rows = request.getParameter("rows");
-		int start = (Integer.parseInt(parameter)-1)*Integer.parseInt(rows);
-		List<MenuBean> query = menu.query(start,Integer.parseInt(rows));
-		try {
-			int count = menu.getCount();
-			response.getWriter().print("{\"total\":"+count+",\"rows\":"+ JSON.toJSONString(query)+"}");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+//	public void getMenus(HttpServletRequest request, HttpServletResponse response,MenuService menuServlet){
+//		String mealId = request.getParameter("mealId") == null ? "0" : request.getParameter("mealId");
+//		String mealName = request.getParameter(" mealName") == null ? null : request.getParameter(" mealName").equals("") ? null : request.getParameter(" mealName");
+//		String seriesName = request.getParameter("seriesName") == null ? null : request.getParameter("seriesName").equals("") ? null : request.getParameter("seriesName");
+//		String mealPrice = request.getParameter("mealPrice") == null ? null : request.getParameter("mealPrice").equals("") ? null : request.getParameter("mealPrice");
+//		String mealImage = request.getParameter("mealImage") == null ? null : request.getParameter("mealImage").equals("") ? null : request.getParameter("mealImage");
+//		String mealSummarize = request.getParameter("mealSummarize") == null ? null : request.getParameter("mealSummarize").equals("") ? null : request.getParameter("mealSummarize");
+//		System.out.println("result ");
+//		System.out.println(mealId);
+//		System.out.println(mealName);
+//		System.out.println(seriesName);
+//		System.out.println(mealPrice);
+//		System.out.println(mealImage);
+//		System.out.println(mealSummarize);
+//		List<MenuBean> query =menuServlet.query(new MenuBean(Integer.parseInt(id),loginName,createTime,trueName,phone,email,address,Double.parseDouble(money)));
+//
+//		Gson gson = new GsonBuilder().create();
+//		String result = gson.toJson(query);
+//		System.out.println("result " + result);
+//
+//		try {
+//			response.getWriter().write(result);
+//			response.getWriter().flush();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 }
